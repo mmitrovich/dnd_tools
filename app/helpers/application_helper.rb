d09b15d59fe5aab6b0708880970fcd3f31b08d49
Mike Mitrovich <mmitrovich@gmail.com>
2017-05-31 22:55:05 +0000

    refactor raw html descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
 	def error_messages_for(object)
 		render(:partial => 'application/error_messages', :locals => {:object => object})
 	end
+
+	def html_description(html_text)
+		if html_text
+			raw sanitize html_text, tags: %w(strong em br p ul ol li), attributes: %w(href)
+		else
+			false
+		end
+	end
 end
