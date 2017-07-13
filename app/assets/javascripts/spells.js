c5ecdd400d8685326336f0934e20f0e1041b1af4
Mike Mitrovich <mmitrovich@gmail.com>
2017-07-13 15:27:36 +0000

    fix feat sorting

--- app/assets/javascripts/spells.js
+++ app/assets/javascripts/spells.js
@@ -23,13 +23,13 @@ function sortTable(n) {
       /*check if the two rows should switch place,
       based on the direction, asc or desc:*/
       if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        if (x.innerText.toLowerCase() > y.innerText.toLowerCase()) {
           //if so, mark as a switch and break the loop:
           shouldSwitch= true;
           break;
         }
       } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        if (x.innerText.toLowerCase() < y.innerText.toLowerCase()) {
           //if so, mark as a switch and break the loop:
           shouldSwitch= true;
           break;
